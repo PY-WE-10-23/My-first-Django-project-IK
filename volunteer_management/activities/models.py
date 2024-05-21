ff18e5fa34f31b704a4eb2ed60838f54a2e4c56c
Iva Kantorova <iva.kantorova@gmail.com>
2024-05-22 00:11:43 +0200

    Navrat na login

--- volunteer_management/activities/models.py
+++ volunteer_management/activities/models.py
@@ -1,21 +1,18 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
 
 class Activity(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     date = models.DateField()
-    location = models.CharField(max_length=100)
-    available_slots = models.PositiveIntegerField()
+    available_slots = models.IntegerField()
 
     def __str__(self):
         return self.title
 
 class Registration(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
     activity = models.ForeignKey(Activity, on_delete=models.CASCADE)
-    registered_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'{self.user.username} - {self.activity.title}'
-
+        return f"{self.user.username} - {self.activity.title}"
